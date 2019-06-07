08faa7649e201d476a1e185b5991deed1ecdd89b
Rohan Panchal <rohan@rohanpanchal.com>
2019-06-07 09:33:39 -0500

    Initial template structure with install script.

---
+++ Templates/SwiftUI.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,20 @@
+//___FILEHEADER___
+
+import Foundation
+import SwiftUI
+
+struct ___VARIABLE_productName___: View {
+    
+    var body: some View {
+        Text("Hello World!")
+    }
+}
+
+#if DEBUG
+struct ___VARIABLE_productName____Previews : PreviewProvider {
+    static var previews: some View {
+        
+        return ___VARIABLE_productName___()
+    }
+}
+#endif
